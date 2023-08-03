54baaca4e158574ce02df7897346b0f9a025f006
Pranshu <pranshgupta54@gmail.com>
2023-08-04 02:57:02 +0530

    cc and cf to db

---
+++ backend/models/contest/Contest.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const contestSchema = new mongoose.Schema({
+    host: {
+        type: String,
+        required: [true, "Host is required."],
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required."],
+    },
+    vanity: {
+        type: String,
+        required: [true, "Vanity is required."],
+    },
+    url: {
+        type: String,
+        required: [true, "URL is required."],
+        unique: true,
+    },
+    startTimeUnix: {
+        type: Number,
+        required: [true, "Start time is required."],
+    },
+    duration: {
+        type: Number,
+        required: [true, "Duration is required in min."],
+    },
+});
+
+const Contest = mongoose.model('Contest', contestSchema);
+
+module.exports = Contest;
