fe9fda3778b167068378ffc5f361d981b68cead4
Pranshu <pranshgupta54@gmail.com>
2023-08-05 17:33:36 +0530

    GET contests fix
    Returns all for no query

--- backend/routes/contest/contestRoutes.js
+++ backend/routes/contest/contestRoutes.js
@@ -13,22 +13,33 @@ router.get("/", async (req, res) => {
         const vanityArray = vanity ? vanity.split(",") : [];
 
         const contests = await contestController.getContestList();
-        const filteredContests = contests.filter((contest) => {
-            // Check if the contest 'host' OR 'vanity' matches the provided parameters
-            return (
-                platformArray.includes(contest.host) ||
-                vanityArray.includes(contest.vanity)
-            );
-        });
 
-        const totalContests = filteredContests.length;
+        // Check if there are any query parameters
+        if (host || vanity) {
+            // If there are query parameters, filter the contests
+            const filteredContests = contests.filter((contest) => {
+                // Check if the contest 'host' OR 'vanity' matches the provided parameters
+                return (
+                    platformArray.includes(contest.host) ||
+                    vanityArray.includes(contest.vanity)
+                );
+            });
 
-        // Create the response object with total and results fields
-        const response = {
-            total: totalContests,
-            results: filteredContests
-        };
-        res.json(response);
+            const totalContests = filteredContests.length;
+
+            // Create the response object with total and results fields
+            const response = {
+                total: totalContests,
+                results: filteredContests
+            };
+            res.json(response);
+        } else {
+            // If no query parameters are provided, return all contests
+            res.json({
+                total: contests.length,
+                results: contests
+            });
+        }
     } catch (err) {
         console.log("Error:", err);
     }
