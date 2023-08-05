d2656791dc3e68d4ac2ab8f7707b86b45181d622
Pranshu <pranshgupta54@gmail.com>
2023-08-06 02:34:47 +0530

    memo for card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import Button from './Button';
 import './css/Card.css'
 import geeksforgeeks from '../assets/geeksforgeeks.svg'
@@ -46,7 +46,7 @@ console.log("rendering");
   );
 }
 
-export default Card;
+export default memo(Card);
 
 
 function updateTimer(startTime) {
