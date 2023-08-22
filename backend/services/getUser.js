48868ca6d8f4540389afdb53197e57e8582d5783
Pranshu <pranshgupta54@gmail.com>
2023-08-22 23:06:24 +0530

    formatting

--- backend/services/getUser.js
+++ backend/services/getUser.js
@@ -2,15 +2,15 @@ const User = require('../models/user/User');
 
 const getUser = async (identifier) => {
     try {
-      const user = await User.findOne({
-        $or: [{ username: identifier }, { email: identifier }]
-      });
-      return user;
+        const user = await User.findOne({
+            $or: [{ username: identifier }, { email: identifier }]
+        });
+        return user;
     } catch (error) {
-      throw new Error('User not found');
+        throw new Error('User not found');
     }
-  };
-  
+};
+
 
 module.exports = {
     getUser
