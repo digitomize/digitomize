a288389755ca1d1ba8fa17aafa52b5c1fe0e2d68
Saurav <er.sauravgpt@gmail.com>
2023-09-01 11:04:36 +0530

    minor changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./src/components/*.{js,ts,jsx,tsx}", "./src/user/**/*.{js,ts,jsx,tsx}",'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',],
+  content: ["./index.html", "./src/components/*.{js,ts,jsx,tsx}", "./src/user/**/*.{js,ts,jsx,tsx}",'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',],
   theme: {
     extend: {
+      fontFamily: {
+        'mainPageText': ['Outfit', 'sans-serif'],
+      },
       colors: {
           cardsColor:'#191919',
      
