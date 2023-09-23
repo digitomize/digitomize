bb4cd6d5959a23a57b13801b42c91e834f869f69
Pranshu <pranshgupta54@gmail.com>
2023-09-23 19:00:10 +0530

    minor fix

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -76,9 +76,9 @@ function updateTimer(startTime, duration) {
   // const currentTime = getCurrentTimeIST();
   const timeDiff = startTime - currentTime;
 
-  if ((duration + startTime) < currentTime) {
+  if ((duration*60 + startTime) < currentTime) {
     return <p>the contest has ended</p>;
-  } else if (startTime <= currentTime && currentTime <= (duration + startTime)) {
+  } else if (startTime <= currentTime && currentTime <= (duration*60 + startTime)) {
     return <p>the contest has started!</p>;
   } else {
     const days = Math.floor(timeDiff / 86400);
