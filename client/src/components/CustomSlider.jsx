7cf9319a00c97c77bd7f3b157493a735f8f9f5c9
Ritik <shrestharitik@gmail.com>
2023-10-03 20:04:17 +0545

    filter alignment

--- client/src/components/CustomSlider.jsx
+++ client/src/components/CustomSlider.jsx
@@ -19,11 +19,15 @@ export default function CustomSlider({ setRange, maxValue }) {
 
   return (
     maxValue > 0 && (
-      <div className="flex  justify-ceenter items-center mr-5 ">
-        <h2 className="text-1xl mr-4 mb-2">Duration (min):</h2>
-        <Box sx={{ width: 300 }}>
+      <div className="flex flex-col md:flex-row justify-center  items-center md:mr-5 ">
+        <h2 className="text-1xl mr-4 mb-2 mt-5 md:mt-0">Duration (min):</h2>
+        <Box
+          className="px-6 py-0 md:py-3 rounded-lg flex flex-col justify-center items-center bg-filter  "
+          sx={{ width: 300 }}
+        >
           <Slider
             max={maxValue}
+            step={15}
             getAriaLabel={() => "Duration range"}
             value={value}
             onChange={handleChange}
