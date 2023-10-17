72ad6737b35c39c3fa856b652a3f69e7e6fe8995
Pranshu <pranshgupta54@gmail.com>
2023-10-18 00:27:51 +0530

    service

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from './App'
 
@@ -12,3 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
+
+serviceWorkerRegistration.register();
