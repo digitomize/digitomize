12f3e1fd7d5641cb62192c4e0a9806a4748cad44
Khagesh Sharma <khageshsharma24@gmail.com>
2023-10-22 01:55:46 +0530

    Add install pwa custom notification

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,11 @@ import App from './App'
 
 import "./index.css"
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
+import InstallPWAButton from './components/InstallPWAButton';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <InstallPWAButton />
     <App />
   </React.StrictMode>
 )
