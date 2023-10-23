8e93bcf2241d5db49f6dc17595ae2888de1ceec2
Anurag Sharma <sharma.anurag0225@gmail.com>
2023-10-24 02:11:17 +0530

    alert added in verion2

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,17 +1,48 @@
 import NewNavbar from "./NewNavbar"
-
+import Alert from '@mui/material/Alert';
 import NewFooter from "./NewFooter"
+import { useState } from "react";
 
 import { Analytics } from '@vercel/analytics/react';
 import { Outlet } from "react-router-dom"
 
 export default function Layout() {
+  const [alertOpen, setAlertOpen] = useState(true);
+
+  const handleCloseAlert = () => {
+
+    setAlertOpen(false);
+  };
 
   return (
     <>
       <NewNavbar />
+      {alertOpen && (
+        <Alert
+          onClose={handleCloseAlert}
+          severity="success"
+          sx={{
+            position: "fixed",
+            zIndex: 200,
+            marginBottom: 2,
+            marginTop: "2%",
+            right: 0
+          }}
+          icon={false}
+        >
+          Haven't checked out the version-2 !? Here it is :-
+          <a
+            href="https://v2.digitomize.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', color: 'blue' }}
+          >
+            https://v2.digitomize.com/
+          </a>
+        </Alert>
+      )}
       <Outlet />
-      <NewFooter/>
+      <NewFooter />
       <Analytics />
     </>
   )
