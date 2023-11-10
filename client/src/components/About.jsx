09b40d5bbe2a9d71ac54daae7e8f081c62b593dd
Pranshu <pranshgupta54@gmail.com>
2023-11-10 18:02:21 +0530

    hyperlinks

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,6 +1,7 @@
 import { AiFillStar, AiOutlineUsergroupAdd, AiFillLinkedin } from "react-icons/ai";
 import { Avatar, Skeleton } from "@mui/material";
 import NewFooter from "./NewFooter";
+import { Link } from "react-router-dom";
 export default function About() {
     return (
         <>
@@ -115,33 +116,38 @@ export default function About() {
                         <div className="stat-value text-secondary">11.5k+</div>
                         {/* <div className="stat-desc">21% more than last month</div> */}
                     </div>
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg> */}
-                            <AiFillLinkedin className="w-8 h-8" />
+                    <Link to={"https://www.linkedin.com/company/digitomize"} className="stat p-0">
+                        <div className="stat">
+                            <div className="stat-figure text-secondary">
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg> */}
+                                <AiFillLinkedin className="w-8 h-8" />
+                            </div>
+                            <div className="stat-title">linkedin followers</div>
+                            <div className="stat-value text-secondary">400+</div>
+                            {/* <div className="stat-desc">21% more than last month</div> */}
                         </div>
-                        <div className="stat-title">linkedin followers</div>
-                        <div className="stat-value text-secondary">400+</div>
-                        {/* <div className="stat-desc">21% more than last month</div> */}
-                    </div>
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg> */}
-                            <AiFillStar className="w-8 h-8" />
+                    </Link>
+                    <Link to={"https://github.com/digitomize/digitomize/stargazers"} className="stat p-0">
+                        <div className="stat">
+                            <div className="stat-figure text-secondary">
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg> */}
+                                <AiFillStar className="w-8 h-8" />
+                            </div>
+                            <div className="stat-title">Github Stars</div>
+                            <div className="stat-value text-secondary">70+</div>
+                            {/* <div className="stat-desc">21% more than last month</div> */}
                         </div>
-                        <div className="stat-title">Github Stars</div>
-                        <div className="stat-value text-secondary">70+</div>
-                        {/* <div className="stat-desc">21% more than last month</div> */}
-                    </div>
-                    <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            <AiOutlineUsergroupAdd className="w-8 h-8" />
+                    </Link>
+                    <Link to={"https://github.com/digitomize/digitomize#contributors-"} className="stat p-0">
+                        <div className="stat">
+                            <div className="stat-figure text-secondary">
+                                <AiOutlineUsergroupAdd className="w-8 h-8" />
+                            </div>
+                            <div className="stat-title">contributors</div>
+                            <div className="stat-value text-secondary">25+</div>
+                            {/* <div className="stat-desc">21% more than last month</div> */}
                         </div>
-                        <div className="stat-title">contributors</div>
-                        <div className="stat-value text-secondary">25+</div>
-                        {/* <div className="stat-desc">21% more than last month</div> */}
-                    </div>
-
+                    </Link>
                 </div>
                 <div className="phone:w-10/12 mx-auto">
                     <h1 className="w-full text-left py-2">
