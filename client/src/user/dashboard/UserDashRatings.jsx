2ff1b8b77a7bf4737d0171a4a38aa36ed5159dd4
Nishant kumar <sarkaarmilitia@gmail.com>
2023-11-14 14:46:00 +0530

    remove details and fix little bit error of mine

--- client/src/user/dashboard/UserDashRatings.jsx
+++ client/src/user/dashboard/UserDashRatings.jsx
@@ -192,7 +192,7 @@ export default function UserDashRatings() {
 
       <UserDashboard/>
         </div> */}
-        <div className="max-md:py-20 md:py-12 min-h-[60vh] mt-24 w-11/12 m-auto">
+        <div className="max-md:py-20 min-h-[60vh] mt-24 w-11/12 m-auto">
           {/* <div className="w-full flex justify-center md:justify-end mb-12 md:mb-8">
         <Checkbox />
       </div> */}
@@ -202,18 +202,24 @@ export default function UserDashRatings() {
 
 
           <ul className="menu menu-xs bg-base-200 rounded-lg w-fit mx-auto my-auto md:scale-150">
-
-            <li><a>
+            
+          <li><a>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /></svg>
               account.js
             </a></li>
-            <li>
-              <details open>
-                <summary>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" /></svg>
+            
+          <li>
+            
+              {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="30 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                My ratings
+                </svg> */}
+                  <ul>
+                
+                
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 inline mr-2 -ml-4 "><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" /></svg>
                   My ratings
-                </summary>
-                <ul>
+
+                <ul/>
 
                   {/* codeforces */}
                   <li>
@@ -355,9 +361,12 @@ export default function UserDashRatings() {
                       </ul>
                     </details>
                   </li> */}
-                </ul>
-              </details>
-            </li>
+              <ul/>
+              
+           </ul>   
+         </li>
+         
+          
             <li><a>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /></svg>
               ranks.board
@@ -373,6 +382,9 @@ export default function UserDashRatings() {
             </Form>
 
           </ul>
+
+
+
 
 
 
@@ -491,3 +503,4 @@ export default function UserDashRatings() {
     );
   }
 }
+
