aa2402a227fc52c7ff4c5c8d43cc79b6b4b63411
Pranshu <pranshgupta54@gmail.com>
2023-11-19 22:04:14 +0530

    Update About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -113,7 +113,7 @@ export default function About() {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                         </div>
                         <div className="stat-title">total views</div>
-                        <div className="stat-value text-secondary">11.5k+</div>
+                        <div className="stat-value text-secondary">15k+</div>
                         {/* <div className="stat-desc">21% more than last month</div> */}
                     </div>
                     <Link to={"https://www.linkedin.com/company/digitomize"} className="stat p-0">
@@ -123,7 +123,7 @@ export default function About() {
                                 <AiFillLinkedin className="w-8 h-8" />
                             </div>
                             <div className="stat-title">linkedin followers</div>
-                            <div className="stat-value text-secondary">400+</div>
+                            <div className="stat-value text-secondary">500+</div>
                             {/* <div className="stat-desc">21% more than last month</div> */}
                         </div>
                     </Link>
@@ -134,7 +134,7 @@ export default function About() {
                                 <AiFillStar className="w-8 h-8" />
                             </div>
                             <div className="stat-title">Github Stars</div>
-                            <div className="stat-value text-secondary">70+</div>
+                            <div className="stat-value text-secondary">75+</div>
                             {/* <div className="stat-desc">21% more than last month</div> */}
                         </div>
                     </Link>
@@ -144,7 +144,7 @@ export default function About() {
                                 <AiOutlineUsergroupAdd className="w-8 h-8" />
                             </div>
                             <div className="stat-title">contributors</div>
-                            <div className="stat-value text-secondary">25+</div>
+                            <div className="stat-value text-secondary">30+</div>
                             {/* <div className="stat-desc">21% more than last month</div> */}
                         </div>
                     </Link>
