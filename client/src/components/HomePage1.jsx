afccb60c6df6ddf8b4cebcebb9ad56e9d694bbe0
Pranshu <pranshgupta54@gmail.com>
2023-11-28 03:21:08 +0530

    supporters section

--- client/src/components/HomePage1.jsx
+++ client/src/components/HomePage1.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-scroll";
 import gitbookSvg from '../public/svg/GitBook.svg'
-import microsoftLogo from '../public/png/MS_Startups_Celebration_Badge_Dark.png'
+import MSMESvg from '../public/svg/MSME.svg'
+import microsoftLogo from '../public/png/MS_Startups_Celebration_Badge_Light.png'
 
 export default function Home1() {
   return (
@@ -22,7 +23,7 @@ export default function Home1() {
         <div className="text-center phone:text-4xl max-phone:text-2xl my-4 lowercase py-4 text-tagColor max-md:flex flex-col font-myFont ">
           <span> Elevate Your Coding and Development Journey</span>{" "}
           <span> with </span> <span>Digitomize</span>
-          <Link to="second">
+          {/* <Link to="second">
             <div className="lower text-center pt-8 animate-bounce my-16">
               <div className="indicator pt-4">
                 <span></span>
@@ -32,39 +33,49 @@ export default function Home1() {
                 <span></span>
               </div>
             </div>
-          </Link>
+          </Link> */}
         </div>
-      </div>
-      <section className="w-full flex justify-center">
 
-      <div className="border-t w-3/4 border-jet bg-dark pt-8">
-        <p className="w-full text-center uppercase text-4xl font-normal tracking-wide">We are trusted by</p>
-        <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20 py-20">
-          <div className="grid grid-cols-1 gap-y-12 md:grid-cols-2 md:gap-x-6 ">
-            {/* map */}
-            <div className="text-center md:flex md:flex-col md:items-center md:justify-center  md:text-center">
-              <div className="md:flex-shrink-0 flex justify-center">
-                <div className="flex items-center justify-center ">
-                  <img src={gitbookSvg} className="w-3/4" alt="" />
-                </div>
-              </div>
-              <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-                <p className="mt-3 text-small tracking-wide text-muted-foreground">community partner</p>
-              </div>
-            </div>
-            <div className="text-center md:flex md:items-center md:justify-center  md:text-center">
-              <div className="md:flex-shrink-0 flex justify-center">
-                <div className="flex items-center justify-center ">
-                  <img src={microsoftLogo} className=" w-3/4 max-w-md" alt="" />
+        <section className="w-full flex justify-center">
+          <div className="border-t md:w-3/4 border-jet bg-dark phone:mt-12">
+            <p className="w-full text-center lowercase text-4xl font-normal tracking-wide pt-8">Supported by</p>
+            <div className="mx-auto w-full max-w-screen-xl md:px-12 py-12">
+              <div className="flex md:flex-row px-3">
+                {/* map */}
+                <div className="text-center flex md:flex-col items-center md:justify-center  md:text-center">
+                  <div className="md:flex-shrink-0 flex justify-center">
+                    <a href="https://www.gitbook.com/" target="_blank">
+                      <div className="flex items-center justify-center ">
+                        <img src={gitbookSvg} className="max-phone:w-full w-3/4" alt="" />
+                      </div>
+                    </a>
+                  </div>
 
                 </div>
+                <div className="text-center md:flex md:items-center md:justify-center  md:text-center">
+                  <div className="md:flex-shrink-0 flex justify-center">
+                    <a href="https://www.microsoft.com/en-us/startups" target="_blank">
+                      <div className="flex items-center justify-center ">
+                        <img src={microsoftLogo} className="w-3/4 max-w-md" alt="" />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="text-center md:flex md:items-center md:justify-center  md:text-center">
+                  <div className="md:flex-shrink-0 flex justify-center">
+                    <a href="https://msme.gov.in/" target="_blank">
+                      <div className="flex items-center justify-center ">
+                        <img src={MSMESvg} className=" w-2/4 max-w-md" alt="" />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                {/* map end */}
               </div>
             </div>
-            {/* map end */}
           </div>
-        </div>
+        </section>
       </div>
-      </section>
     </>
   );
 }
