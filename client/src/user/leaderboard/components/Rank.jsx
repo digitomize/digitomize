fe9c5fd833ffea07c1c88b46efa25c549fdf24c0
Tushar Daiya <tushardaiya@outlook.com>
2023-12-18 08:27:15 +0530

    added skeleton for ranks loading

--- client/src/user/leaderboard/components/Rank.jsx
+++ client/src/user/leaderboard/components/Rank.jsx
@@ -1,6 +1,7 @@
 import { EmojiEvents, OpenInNew } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import boy from "../../../assets/boyV7.png";
+import { Skeleton } from "@mui/material";
 
 const Rank = ({ color, pt = 8, user }) => {
   return (
@@ -24,13 +25,34 @@ const Rank = ({ color, pt = 8, user }) => {
         </div>
         <div className="name">
           <div className="flex flex-row pt-4 justify-center">
-            <h2 className="text-center text-light-blue">
-              {user?.name || "loading..."}
+            {user?.name ? (
+              <h2 className="text-center text-light-blue">
+                {user?.name}
+                <OpenInNew fontSize="small" />
+              </h2>
+            ) : (
+              <Skeleton
+                variant="rounded"
+                width={"4rem"}
+                height={"1rem"}
+                sx={{ bgcolor: "grey.600" }}
+                className="my-2"
+              />
+            )
+            }
+            {/* <h2 className="text-center text-light-blue">
+              {user?.name || <Skeleton
+                        variant="rounded"
+                        width={"4rem"}
+                        height={"2rem"}
+                        sx={{ bgcolor: "grey.600" }}
+                        className="my-2"
+                      />}
             </h2>
-            <OpenInNew fontSize="small" />
+            <OpenInNew fontSize="small" /> */}
           </div>
           <p className="text-center text-light-blue text-xs max-phone:hidden">
-            {`(@${user?.username || "loading..."})`}
+            {user?.username&&`(@${user?.username || "loading..."})`}
           </p>
         </div>
       </Link>
