9374c083502a3bac85ffb1c9b9ed4fc45e630cc1
im-vedant <194vedantgupta@gmail.com>
2023-12-22 22:17:30 +0530

    added platform filter

--- client/src/user/leaderboard/Leaderboard.jsx
+++ client/src/user/leaderboard/Leaderboard.jsx
@@ -5,6 +5,11 @@ import {
   useNavigate,
   useSearchParams,
 } from "react-router-dom";
+import {
+  leetcode,
+  codechef,
+  codeforces,
+} from "../../components/AllAssets";
 import NewNavbar from "../../components/globals/NewNavbar";
 import { leaderboardData, rankOnLeaderboard } from "../../../api";
 import { OpenInNew, WorkspacePremium, Info } from "@mui/icons-material";
@@ -15,6 +20,13 @@ import {
   Tooltip,
   tooltipClasses,
   styled,
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  ListItemIcon,
+  ListItemText
 } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -37,6 +49,25 @@ export default function Leaderboard() {
   const [currentUserData, setCurrentUserData] = useState(null);
   const { userDetails } = useUserDetails();
   const [top3, setTop3] = useState([]);
+  const [selectedPlatform, setSelectedPlatform] = useState('')
+
+  const handleChange = (event) => {
+    setSelectedPlatform(event.target.value);
+    setSearchParams({ platform : event.target.value
+    })
+    fetchLbData()
+  };
+
+  const platforms = [
+    "leetcode",
+    "codechef",
+    "codeforces"
+  ];
+  const platformsIcon = [
+    leetcode,
+    codechef,
+    codeforces
+  ];
   // console.log("USERERER", userDetails);
   const HtmlTooltip = styled(({ className, ...props }) => (
     <Tooltip className="custom-bg" {...props} classes={{ popper: className }} />
@@ -115,12 +146,18 @@ export default function Leaderboard() {
     try {
       // console.log(currentPage);
       setLoading(true);
-      const res = await leaderboardData(currentPage);
-      setTotalPages(res.data.total_pages);
-      setData(res.data.leaderboard);
-      setTop3(res.data.top3);
-      console.log(res.data);
-      console.log(res.data.leaderboard);
+      let res;
+      if(searchParams.platform)
+      res = await fetch(`https://www.v2api.digitomize.com/user/leaderboard?platform=${searchParams.platform}?page=${page}`)
+      else
+      res= await fetch("https://www.v2api.digitomize.com/user/leaderboard")
+
+      const data =await res.json()
+      setTotalPages(data.total_pages);
+      setData(data.leaderboard);
+      setTop3(data.top3);
+      console.log(data);
+      console.log(data.leaderboard);
       console.log(top3[0]);
     } catch (err) {
       console.log(err);
@@ -135,24 +172,62 @@ export default function Leaderboard() {
 
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
-
-    navigate(`${location.pathname}?page=${value}`);
+    if(searchParams.get(platform))
+    navigate(`${location.pathname}?page=${value}?platform=${searchParams.get(platform)}`);
+    else
+    navigate(`${location.pathname}?page=${value}`)
   };
 
   const getRank = (index) => {
     return index + 4 + (currentPage - 1) * 5;
   };
-
   return (
     <>
       <NewNavbar position="static" />
-      <div className="heading text-center my-4">
-        <h1>Leaderboard</h1>
+      <div className="text-white text-center my-4 flex flex-col items-center justify-center">
+        <h1>One Scoreboard for<br />All Your <span className="bg-[#1584FF] py-1">&nbsp;Coding Battles&nbsp;</span></h1>
+        <div className="border border-[#25478B] bg-[#004CE454] rounded-lg w-fit px-4 py-1 mt-3 flex flex-row items-center justify-center">
+          Share the board now
+          <svg className="ml-2 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 12 12" fill="#fff">
+            <path d="M10 12C9.44444 12 8.97222 11.825 8.58333 11.475C8.19444 11.125 8 10.7 8 10.2C8 10.13 8.00556 10.0574 8.01667 9.9822C8.02778 9.907 8.04445 9.8396 8.06667 9.78L3.36667 7.32C3.17778 7.47 2.96667 7.5876 2.73333 7.6728C2.5 7.758 2.25556 7.8004 2 7.8C1.44444 7.8 0.972222 7.625 0.583333 7.275C0.194444 6.925 0 6.5 0 6C0 5.5 0.194444 5.075 0.583333 4.725C0.972222 4.375 1.44444 4.2 2 4.2C2.25556 4.2 2.5 4.2426 2.73333 4.3278C2.96667 4.413 3.17778 4.5304 3.36667 4.68L8.06667 2.22C8.04445 2.16 8.02778 2.0926 8.01667 2.0178C8.00556 1.943 8 1.8704 8 1.8C8 1.3 8.19444 0.875 8.58333 0.525C8.97222 0.175 9.44444 0 10 0C10.5556 0 11.0278 0.175 11.4167 0.525C11.8056 0.875 12 1.3 12 1.8C12 2.3 11.8056 2.725 11.4167 3.075C11.0278 3.425 10.5556 3.6 10 3.6C9.74444 3.6 9.5 3.5576 9.26667 3.4728C9.03333 3.388 8.82222 3.2704 8.63333 3.12L3.93333 5.58C3.95556 5.64 3.97222 5.7076 3.98333 5.7828C3.99444 5.858 4 5.9304 4 6C4 6.07 3.99444 6.1426 3.98333 6.2178C3.97222 6.293 3.95556 6.3604 3.93333 6.42L8.63333 8.88C8.82222 8.73 9.03333 8.6126 9.26667 8.5278C9.5 8.443 9.74444 8.4004 10 8.4C10.5556 8.4 11.0278 8.575 11.4167 8.925C11.8056 9.275 12 9.7 12 10.2C12 10.7 11.8056 11.125 11.4167 11.475C11.0278 11.825 10.5556 12 10 12Z" fill="white" />
+          </svg>
+        </div>
       </div>
       <div className="flex justify-center max-phone:gap-6 phone:gap-12 phone:w-4/6 w-11/12 mx-auto mt-8 h-fit">
         <Rank color="#C0C0C0" user={top3[1]} />
         <Rank color="#FFD700" pt="0" user={top3[0]} />
         <Rank color="#CD7F32" user={top3[2]} />
+      </div>
+      <div className="phone:w-4/6 w-11/12 mx-auto">
+      <Box className={"w-[170px] flex ml-auto"} >
+      <FormControl className={"bg-[#474748] rounded-[8px] w-full text-[16px]"} variant="outlined">
+        <InputLabel id="demo-simple-select-label" className={"text-white text-[16px]"} >select platform</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={selectedPlatform}
+          label="select platform"
+          onChange={handleChange}
+          className={"bg-[#474748] text-white"}
+          sx={{ display: "flex", flexDirection :'row', gap: 0 }}
+        >  {platforms.map((platform, idx) => (
+          <MenuItem key={platform} value={platform} >
+            <ListItemIcon>
+              <img className="flex"
+                src={platformsIcon[idx]}
+                alt="a"
+                style={{
+                  width: "20px",
+                  height: "20px",
+                }}
+              />
+            </ListItemIcon>
+            <ListItemText primary={platform} />
+          </MenuItem>
+        ))}
+        </Select>
+      </FormControl>
+    </Box>
       </div>
       <div className="phone:w-4/6 w-11/12 mx-auto mt-4 text-center">
         <div className="overflow-x-auto border-2 border-[#D1E5F4] rounded-xl shadow-[9px_9px_0px_#D1E5F4]">
