ca7140df448f7471e627be98db3fce34979a9ae8
im-vedant <194vedantgupta@gmail.com>
2024-01-01 17:14:03 +0530

    Removed unwanted code and restructured the code

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -17,30 +17,6 @@ export default function About() {
         <div className="hero">
           <div className="hero-content flex-col sm:flex-row">
             <div className="left text-center flex flex-col justify-center items-center">
-              {/* <div className="text-center w-fit">
-                <h1 className="lg:text-[83px] sm:text-[50px] md:text-[61px] phone:text-[30px] items-center text-white justify-center  max-phone:text-5xl my-0 font-medium flex phone:flex-row max-phone:flex-col mx-auto w-full">
-                  An{" "}
-                  <span className="bg-digitomize-bg phone:hidden py-[2px] sm:py-1 rounded-md font-bold sm:ml-2 w-fit">
-                    Open
-                  </span> <span className="bg-digitomize-bg phone:hidden py-[2px] sm:py-1 my-1 rounded-md font-bold sm:mr-2 w-fit">
-                    Source
-                  </span>
-                  <span className="bg-digitomize-bg max-phone:hidden py-[2px] sm:py-1 rounded-md font-bold mx-2">
-                    Open Source
-                  </span>
-                  {" "}
-
-                  project
-                </h1>
-                <p className="py-6 font-['Source Code Pro'] phone:text-4xl phone:text-right text-xl">
-                  by and for the coders
-                </p>
-              </div> */}
-              {/* <div>
-                                <p className="text-xl my-4 w-3/5 mx-auto">
-                                    Digitomize is designed to provide developers with a comprehensive platform for exploring coding contests and creating dynamic portfolios that showcase their coding skills and achievements.
-                                </p>
-                            </div> */}
               <div className="w-full flex flex-row justify-between  text-white mt-[80px]">
                 <div className="flex flex-col lg:max-w-[50%] text-left">
                   <div className="mb-[50px]">
@@ -123,87 +99,12 @@ export default function About() {
                 </h2>
                 <MuiTimeline></MuiTimeline>
               </div>
-              {/* <div className="flex flex-col md:w-4/5 lg:w-3/5  phone:w-3/5">
-                <div className="one flex md:flex-row max-md:flex-col">
-                  <div className="left md:w-2/5">
-                    <h1 className="m-0 md:text-left">contests</h1>
-                  </div>
-                  <div className="right md:w-3/5 md:text-left">
-                    Explore upcoming coding contests from platforms like
-                    LeetCode, Codeforces, CodeChef, and more. Filter contests
-                    based on various criteria and view contest details such as
-                    remaining time, duration, and more
-                  </div>
-                </div>
-                <div className="two flex md:flex-row max-md:flex-col mt-4">
-                  <div className="left md:w-2/5">
-                    <h1 className="m-0 md:text-left">portfolio</h1>
-                  </div>
-                  <div className="right md:w-3/5 md:text-left">
-                    Create your developer portfolio by providing user handles
-                    for different coding platforms. The platform dynamically
-                    fetches and displays your updated ratings, achievements, and
-                    coding history.
-                  </div>
-                </div>
-              </div> */}
+             
 
               <div className="divider phone:w-2/5 max-phone:w-3/5 mx-auto"></div>
               <div className="w-[80vw]">
                 <Contributors />
               </div>
-
-              {/* <div className="lg:hidden card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-2">
-                                <div className="">
-                                    <div className="mockup-browser border bg-base-300">
-                                        <div className="mockup-browser-toolbar">
-                                            <div
-                                                className="input text-custom-blue"
-                                                style={{ width: "fit-content" }}
-                                            >
-                                                digitomize.com/profile
-                                            </div>
-                                        </div>
-                                        <div className="flex flex-row px-4 py-4 bg-base-200">
-                                            <div className="flex flex-col w-2/4">
-                                                <Avatar
-                                                    src="/src/assets/hacker.png"
-                                                    className="bg-custom-blue mb-2 self-center"
-                                                    sx={{ padding: "6%", width: 40, height: 40 }}
-                                                />
-                                                <Skeleton
-                                                    variant="text"
-                                                    sx={{ fontSize: "0.8rem", bgcolor: "grey.600" }}
-                                                />
-                                                <Skeleton
-                                                    variant="text"
-                                                    sx={{ fontSize: "0.8rem", bgcolor: "grey.600" }}
-                                                />
-                                                <Skeleton
-                                                    variant="text"
-                                                    sx={{ fontSize: "0.8rem", bgcolor: "grey.600" }}
-                                                />
-                                            </div>
-                                            <div className="flex flex-col w-2/4">
-                                                <Skeleton
-                                                    variant="rounded"
-                                                    sx={{ bgcolor: "grey.600" }}
-                                                    width={"100%"}
-                                                    height={"60%"}
-                                                    className="mx-2"
-                                                />
-                                                <Skeleton
-                                                    variant="rounded"
-                                                    sx={{ bgcolor: "grey.600" }}
-                                                    width={"100%"}
-                                                    height={"70%"}
-                                                    className="mt-2 mx-2"
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> */}
             </div>
           </div>
         </div>
