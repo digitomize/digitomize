4ae8a4ead90e3f53947c43e699460a333c418cb0
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2024-01-10 18:50:44 +0530

    modularised the svg generation in backend, now new svg card for some other platform can easily be added into it

--- backend/users/controllers/generateSVG.js
+++ backend/users/controllers/generateSVG.js
@@ -1,21 +1,15 @@
 import { getUser } from "../services/getUser.js";
-import { leetcodeSVG } from "../utils/leetcodeSVG.js";
-import { codechefSVG } from "../utils/codechefSVG.js";
-import { codeforcesSVG } from "../utils/codeforcesSVG.js";
+import { svgCard } from "../utils/svgCard.js";
 
 const generateSVG = async (req, res) => {
   try {
     const user = await getUser(req.params.username);
-    // console.log(user);
 
     const queries = req.query;
-    // console.log(queries);
     const platforms = ["leetcode", "codechef", "codeforces"];
     let toReturn = [];
     for (const e in queries)
       if (platforms.includes(e) && (queries[e] === "1" || queries[e].includes("1"))) toReturn.push(e);
-
-    // console.log(toReturn);
 
     let cards = `<svg width="100%" height="100%" version="1.1"
     xmlns="http://www.w3.org/2000/svg">`;
@@ -25,13 +19,13 @@ const generateSVG = async (req, res) => {
           let data = user[e];
           let card = ``;
           if (e === "leetcode") {
-            card += leetcodeSVG(data,width+"%",height,x+"%");
+            card += svgCard(data,width+"%",height,x+"%",e);
           }
           if (e === "codechef") {
-            card += codechefSVG(data,width+"%",height,x+"%");
+            card += svgCard(data,width+"%",height,x+"%",e);
           }
           if (e === "codeforces") {
-            card += codeforcesSVG(data,width+"%",height,x+"%");
+            card += svgCard(data,width+"%",height,x+"%",e);
           }
           cards += `${card}`;
           x += 100/toReturn.length;
@@ -55,9 +49,5 @@ const generateSVG = async (req, res) => {
     });
   }
 };
-
-const addGTag = (x) => {
-  return `<g transform="translate(${x},0)">`
-}
 
 export { generateSVG };
---
+++ backend/users/utils/svgCard.js
@@ -0,0 +1,28 @@
+import { codechefLogo } from "./codechefLogo.js";
+import { codeforcesLogo } from "./codeforcesLogo.js";
+import { leetcodeLogo } from "./leetcodeLogo.js";
+import { svgBottom,svgData,svgWrapper } from "./svgUtils.js"
+
+const svgCard = (data,width,height,x,logoType) => {
+    let logo = ``;
+    switch(logoType){
+        case "leetcode":
+            logo = leetcodeLogo();
+            break;
+        case "codeforces":
+            logo = codeforcesLogo();
+            break;
+        case "codechef":
+            logo = codechefLogo();
+            break;
+    }
+    return `
+    ${svgWrapper(width,height,x)}
+        ${logo}
+        ${svgData(data)}
+        ${svgBottom()}
+    </svg>
+    `
+}
+
+export {svgCard}
