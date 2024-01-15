499aa7251d6be2972789b555fe4a75c2914d14a7
Mukund Solanki <mukundsolanki665@gmail.com>
2024-01-15 23:53:48 +0530

    Added the feature to add events to google calendar without using Oauth

--- client/src/components/globals/Card.jsx
+++ client/src/components/globals/Card.jsx
@@ -10,8 +10,8 @@ import {
   atcoder,
 } from "../AllAssets";
 import ShareModel from "../share_model";
-import { IoCalendarNumberOutline } from "react-icons/io5";
-import { ToastContainer, toast } from "react-toastify";
+import moment from "moment-timezone";
+import { IoCalendarNumber } from "react-icons/io5";
 
 const frontendUrl = import.meta.env.VITE_REACT_APP_FRONTEND_URL;
 const hostToSVGMap = {
@@ -23,6 +23,55 @@ const hostToSVGMap = {
   atcoder: atcoder,
   // Add other hosts and their corresponding SVG variables here
 };
+const LOCATION_ID_UTC = 1440;
+
+const generateTimeAndDateURL = (startTimeUnix) => {
+
+    // Convert the Unix timestamp to a datetime in the UTC timezone
+    const utcDateAndTime = moment.tz(startTimeUnix * 1000, "UTC");
+
+    // Get the respective Date and Time Values.
+    const utcStartMonth = utcDateAndTime.format("MM");
+    const utcStartDate = utcDateAndTime.format("DD");
+    const utcStartYear = utcDateAndTime.format("YYYY");
+    const utcStartTime = utcDateAndTime.format("HH:mm:ss");
+    const utcStartHour = utcStartTime.split(":")[0];
+    const utcStartMin = utcStartTime.split(":")[1];
+    const utcStartSec = utcStartTime.split(":")[2];
+  
+    // Form the URL to be directed to when clicked on time.
+    const timeAndDateURL = new URL("https://timeanddate.com/worldclock/fixedtime.html");
+    const params = {
+      day:utcStartDate,
+      month:utcStartMonth,
+      year:utcStartYear,
+      hour:utcStartHour,
+      min:utcStartMin,
+      sec:utcStartSec,
+      p1:LOCATION_ID_UTC,
+    };
+  
+    // Append the respective parameter's to timeanddate's URL.
+    timeAndDateURL.search = new URLSearchParams(params).toString();
+    return timeAndDateURL.href;
+};
+
+const addToGoogleCalendar = ({ name, startTimeUnix, duration, url, host, vanity }) => {
+  const startTime = new Date(startTimeUnix * 1000);
+  const endTime = new Date((startTimeUnix + duration * 60) * 1000);
+
+  const formattedStartTime = startTime.toISOString().replace(/[-:]/g, "");
+  const formattedEndTime = endTime.toISOString().replace(/[-:]/g, "");
+
+  // Encode contest details in the description
+  const description = `Contest Details:%0AName: ${name}%0ADuration: ${duration} minutes%0AHost: ${host}%0AContest URL: ${url}%0A~DIGITOMIZE`;
+
+  const googleCalendarUrl = `https://calendar.google.com/calendar/u/0/r/eventedit?dates=${formattedStartTime}/${formattedEndTime}&text=${encodeURIComponent(name)}&details=${description}`;
+
+  // Open the Google Calendar event creation page in a new tab
+  window.open(googleCalendarUrl, "_blank");
+};
+
 
 function Card({ contest }) {
   const { name, startTimeUnix, url, duration, host, vanity } = contest;
@@ -59,188 +108,6 @@ function Card({ contest }) {
     />
   );
 
-  const gapi = window.gapi;
-  const google = window.google;
-
-  const CLIENT_ID = import.meta.env.VITE_REACT_APP_CLIENT_ID;
-  const API_KEY = import.meta.env.VITE_REACT_APP_CALENDAR_API;
-  const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
-  const SCOPES = "https://www.googleapis.com/auth/calendar";
-
-  const accessToken = localStorage.getItem("access_token");
-  const expiresIn = localStorage.getItem("expires_in");
-
-  let gapiInited = false, gisInited = false, tokenClient;
-
-  useEffect(() => {
-    //const expiryTime = new Date().getTime() + expiresIn * 1000;
-    const initializeGoogleAPIs = async () => {
-      gapiLoaded();
-      gisLoaded();
-    };
-    initializeGoogleAPIs();
-  }, []);
-
-
-  function gapiLoaded() {
-    gapi.load("client", initializeGapiClient);
-  }
-
-  async function initializeGapiClient() {
-    try {
-      await gapi.client.init({
-        apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC],
-      });
-
-      gapiInited = true;
-
-      if (accessToken && expiresIn) {
-        gapi.client.setToken({
-          access_token: accessToken,
-          expires_in: expiresIn,
-        });
-      }
-    } catch (error) {
-      console.error('Error initializing Google API client:', error);
-    }
-  }
-
-  function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES,
-      callback: "", // defined later
-    });
-
-    gisInited = true;
-  }
-
-  function handleAuthClick() {
-    if (!tokenClient) {
-      // If tokenClient is not initialized, call gisLoaded to initialize it
-      // Do not remove this check condition
-      gisLoaded();
-    }
-
-    tokenClient.callback = async (resp) => {
-      try {
-        if (resp.error) {
-          throw new Error(`Authentication error: ${resp.error}`);
-        }
-
-        const { access_token, expires_in } = gapi.client.getToken();
-        if (!access_token || !expires_in) {
-          throw new Error('Invalid access token or expiration time');
-        }
-
-        localStorage.setItem("access_token", access_token);
-        localStorage.setItem("expires_in", expires_in);
-
-        addEventToGoogleCalendar();
-      } catch (error) {
-        console.error('Authentication error:', error);
-        toast.error('Authentication error, please try again!', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      }
-    };
-
-    if (!(accessToken && expiresIn)) {
-      try {
-        tokenClient.requestAccessToken({ prompt: "consent" });
-      } catch (error) {
-        console.error('Error requesting access token:', error);
-      }
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      try {
-        tokenClient.requestAccessToken({ prompt: "" });
-      } catch (error) {
-        console.error('Error requesting access token:', error);
-      }
-    }
-  }
-
-
-  async function addEventToGoogleCalendar() {
-    // const startTime = new Date(startTimeUnix * 1000).toISOString();
-    // const endTime = new Date((startTimeUnix + duration * 60) * 1000).toISOString();
-
-    // console.log(startTime);
-    // console.log(endTime);
-
-    const startTimeUTC = new Date(startTimeUnix * 1000);
-    const startTimeIST = new Date(startTimeUTC.getTime() + (5 * 60 + 30) * 60000).toISOString();
-
-    const endTimeUTC = new Date((startTimeUnix + duration * 60) * 1000);
-    const endTimeIST = new Date(endTimeUTC.getTime() + (5 * 60 + 30) * 60000).toISOString();
-
-
-    console.log(startTimeIST);
-    console.log(endTimeIST);
-
-    const event = {
-      summary: name,
-      start: {
-        dateTime: startTimeIST,
-        timeZone: "Asia/Kolkata",
-      },
-      end: {
-        dateTime: endTimeIST,
-        timeZone: "Asia/Kolkata",
-      },
-      description: `Contest details: ${url}`,
-      "reminders": {
-        "useDefault": false,
-        "overrides": [
-          {
-            "method": "popup",
-            "minutes": 10
-          }
-        ]
-      },
-    };
-
-    try {
-      const response = await gapi.client.calendar.events.insert({
-        calendarId: "primary",
-        resource: event,
-      });
-
-      console.log("Event added to Google Calendar:", response);
-      toast.success('Event added to Google Calendar!', {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    } catch (error) {
-      console.error("Error adding event to Google Calendar:", error);
-      toast.error('Error adding event to Google Calendar, try again!', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  }
-
   return (
     <div
       className="border-[#D1E5F4] border-2 hover:shadow-[8px_8px_0px_#D1E5F4] my-4 sm:w-96 min-h-[250px] p-6 rounded-xl bg-cardsColor flex flex-col hover:scale-[1.02] hover:bg-cardsHover m-1"
@@ -251,7 +118,9 @@ function Card({ contest }) {
           id="startTime"
           className="text-card-text font-light leading-tight lowercase text-lg max-md:text-sm"
         >
-          {startTimeIST}
+          <Link to={timeAndDateURL} className="my-auto underline" target="_blank" rel="noopener noreferrer">
+            {`${startMonth} ${startDate}, ${startYear} at ${startTime}`}
+          </Link>
         </p>
         <img src={hostToSVGMap[host]} alt={host} width="13%" />
       </div>
@@ -284,8 +153,8 @@ function Card({ contest }) {
             </svg>
           </button>
 
-          <button id="authorize_button" onClick={handleAuthClick} aria-label="Authorize Google Calendar Integration">
-            <IoCalendarNumberOutline className="w-7 h-7 ml-4" />
+          <button id="calendarButton" onClick={() => addToGoogleCalendar(contest)} aria-label="Google Calendar Integration">
+            <IoCalendarNumber style={{ color: "white" }} className="w-7 h-7 ml-4" />
           </button>
 
           {show && main_model}
