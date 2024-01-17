c58454c67bd4404e2670f741e25fa83f08a7cb3b
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-01-17 14:02:05 +0530

    Remove unused stats route

--- backend/users/routes/userRoutes.js
+++ backend/users/routes/userRoutes.js
@@ -6,7 +6,6 @@ import { handleUpdateUserProfile } from "../controllers/userUpdateController.js"
 import { addUID } from "../middlewares/authMiddleware.js";
 import { getLeaderboard } from "../controllers/leaderboardController.js";
 import { generateSignature } from "../controllers/cloudinaryUploadController.js";
-import { stats } from "../services/contributorsStatsController.js";
 
 const router = express.Router();
 
@@ -24,8 +23,6 @@ router.post("/dashboard", addUID, handleUpdateUserProfile);
 router.get("/signImageUpload", addUID, generateSignature);
 
 router.get("/profile/:username", handleUserProfilePreview);
-
-router.get("/stats", stats);
 
 // router.post('/profile/:username', addUID, checkUserOwnership, handleUpdateUserProfile);
 
