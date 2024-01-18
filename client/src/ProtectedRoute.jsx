6e403bb78de5ddadf2a0e947cff6e59e6640c975
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-01-18 21:30:27 +0530

    protecting access to authorized pages for unverified users

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useUserAuth } from "./context/UserAuthContext";
+import { auth } from "../firebase";
 
 function ProtectedRoute() {
   const { user } = useUserAuth();
-  return user ? (
+  
+  return user && auth?.currentUser?.emailVerified ? (
     <Outlet />
   ) : (
     <Navigate to="/login?message=Please login first!" />
