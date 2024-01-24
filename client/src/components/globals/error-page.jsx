591b84fcab3cf2d05fa29d6f8a92c9197d0e1e22
Felix Owolabi <owolabifelix78@gmail.com>
2024-01-24 13:14:10 +0100

    refactored css into tailwindcss

--- client/src/components/globals/error-page.jsx
+++ client/src/components/globals/error-page.jsx
@@ -1,5 +1,4 @@
 import { useRouteError, Link } from "react-router-dom";
-import "/src/components/css/error-page.css";
 import Navbar from "./NewNavbar";
 import { Helmet } from "react-helmet";
 import { MetaData } from "../CustomComponents";
@@ -12,15 +11,15 @@ export default function ErrorPage() {
     <>
       <MetaData path="error" />
       <Navbar />
-      <div className="error-container">
-        <div className="error-page">
-          <h1>Oops!</h1>
-          <p>Sorry, an unexpected error has occurred.</p>
-          <p>
+      <div className=" h-80vh flex justify-center items-center justify-items-center">
+        <div className="h-full w-screen flex flex-col items-center justify-center gap-5">
+        <h1 className="text-6xl md:text-[90px]">Oops!</h1>
+          <p className="text-2xl md:text-[40px] text-center">Sorry, an unexpected error has occurred.</p>
+          <p className="text-2xl md:text-[40px] text-center">
             <i>{error.statusText && `Error : ${error.statusText}`}</i>
           </p>
-          <button className="button-4">
-            <Link to="/discord">Report on Discord</Link>
+          <button className="appearance-none bg-gray-100 border border-gray-300 rounded-lg shadow-md box-border text-gray-700 cursor-pointer inline-block font-sans text-sm md:text-base font-semibold leading-5 list-none py-2 px-6 md:py-3 md:px-8 relative transition duration-200 ease-in-out select-none whitespace-nowrap hover:bg-gray-300 hover:text-gray-800 focus:outline-none disabled:bg-gray-100 disabled:border-gray-300 disabled:text-gray-500 disabled:cursor-not-allowed active:bg-gray-200 active:shadow-none ">
+            <Link className="text-gray-900 no-underline text-lg" to="/discord">Report on Discord</Link>
           </button>
         </div>
       </div>
