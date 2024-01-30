4544c1f061f976e2b5b3c5f96a51ecd385430de6
Ansh <kh.anshul04@gmail.com>
2024-01-30 20:50:50 +0530

    removed deprecated module

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import jsconfigPaths from 'vite-jsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),jsconfigPaths()],
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
+  },resolve: {
+    alias: {
+      "@components": "/src/components",
+      // Add other aliases as needed
+    },
   },
 });
