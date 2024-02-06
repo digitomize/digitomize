a293f617fe10bb2a5ee134a326f3fb6760a51f85
Abhishek Girnar <64825778+Abhi1234-tech@users.noreply.github.com>
2024-02-06 11:47:36 +0000

    [BUGFIX]-707

--- client/src/components/globals/Footer.jsx
+++ client/src/components/globals/Footer.jsx
@@ -26,6 +26,11 @@ export default function Footer() {
           >
             Star
           </GitHubButton>
+          <div class="flex justify-center mt-px-2" style={{marginTop:'5px'}}>
+            <a href="https://www.netlify.com">
+              <img src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg" alt="Deploys by Netlify" class="w-[18] h-10" />
+            </a>
+          </div>
         </aside>
         <div className="lg:flex-1 flex flex-col sm:flex-row sm:flex-wrap mx-auto justify-between gap-8 mb-4 sm:mb-0">
           <nav className="flex justify-center items-center sm:!items-start flex-col gap-3 w-full sm:!w-[45%] xl:!w-fit">
