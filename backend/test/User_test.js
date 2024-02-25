40bf92afd9417ef95c5bf2a67412b61e1ccfc412
Ethan Sue <ethans2028@gmail.com>
2024-02-25 12:23:19 -0800

    Added stopwatches to time user model tests

--- backend/test/User_test.js
+++ backend/test/User_test.js
@@ -7,36 +7,44 @@ describe = benchtest.describe(describe);
 
 describe('Create User in Database', () => {
     it('Create a new User and verify UID', (done) => {
-        var newUser = new User({ uid: "tp42182024", username: "Username1", email_verified: true});
-        const expectedUID= "tp42182024";
-        assert.equal(expectedUID, newUser.uid, "UID does not match");
-        done();
+      console.time('Test Time');
+      var newUser = new User({ uid: "tp42182024", username: "Username1", email_verified: true});
+      const expectedUID= "tp42182024";
+      assert.equal(expectedUID, newUser.uid, "UID does not match");
+      console.timeEnd('Test Time');
+      done();
     });
 });
 
 describe('Create User in Database', () => {
     it('Create a new User and verify Username', (done) => {
-        var newUser = new User({ uid: "tp42182024", username: "Username1", email_verified: true});
-        const expectedName= "Username1";
-        assert.equal(expectedName, newUser.username, "Username does not match");
-        done();
+      console.time('Test Time');
+      var newUser = new User({ uid: "tp42182024", username: "Username1", email_verified: true});
+      const expectedName= "Username1";
+      assert.equal(expectedName, newUser.username, "Username does not match");
+      console.timeEnd('Test Time');
+      done();
     });
 });
 
 describe('Create User in Database', () => {
     it('Create a new User and verify Email', (done) => {
-        var newUser = new User({ uid: "tp42182024", username: "Username1", email_verified: true});
-        const expectedName= true;
-        assert.equal(expectedName, newUser.email_verified, "Email was not verifed");
-        done();
+      console.time('Test Time');
+      var newUser = new User({ uid: "tp42182024", username: "Username1", email_verified: true});
+      const expectedName= true;
+      assert.equal(expectedName, newUser.email_verified, "Email was not verifed");
+      console.timeEnd('Test Time');
+      done();
     });
 });
 
 describe('Create User in Database', () => {
     it('Create a new User and verify Email', (done) => {
+        console.time('Test Time');
         var newUser = new User({ uid: "tp42182024", username: "Username1", role: 1, email_verified: true});
         const expectedNumber = 1;
         assert.equal(expectedNumber, newUser.role, "Role number is not valid");
+        console.timeEnd('Test Time');
         done();
     });
 });
