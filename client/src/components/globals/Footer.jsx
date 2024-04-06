ae27d9da7a0c147f68951c46cb17c372b220ed11
Youssef-Elouedi <mohamedyoussef.elouedi@medtech.tn>
2024-04-06 14:38:23 +0100

    in the footer, turn the logo into a navigable button to home

--- client/src/components/globals/Footer.jsx
+++ client/src/components/globals/Footer.jsx
@@ -10,8 +10,15 @@ export default function Footer() {
     <footer className="font-['Geist'] bg-cardsColor pb-10 mt-6">
       <div className="footer lg:max-w-7xl mx-auto p-10 text-base-content md:items-center flex flex-col md:flex-row justify-center items-start">
         <aside className="flex flex-col mx-auto sm:!ml-0 md:!mr-8 text-center sm:text-start mb-4 sm:mb-0">
-          <img src={dynamicLogo} alt="logo" style={{ maxHeight: "7rem", maxWidth: "7rem", marginTop: "-.90rem" }} />
+        <a href="/home">
+          <img
+          src={dynamicLogo}
+          alt="logo"
+          style={{ maxHeight: "7rem", maxWidth: "7rem", marginTop: "-.90rem" }}
+          />
+          </a>
           <h2 className="text-2xl my-3">Digitomize</h2>
+
 
           <p className="text-base-content text-md max-w-[230px] mb-5">
             Explore upcoming coding contests and dynamically create developer
