def288081466a88249c97059b406360bd16845a1
Youssef-Elouedi <97735427+Youssef-Elouedi@users.noreply.github.com>
2024-04-08 15:09:42 +0100

    Update Footer.jsx to fix conflict

--- client/src/components/globals/Footer.jsx
+++ client/src/components/globals/Footer.jsx
@@ -2,6 +2,7 @@ import { dynamicLogo } from "../AllAssets";
 import GitHubButton from "react-github-btn";
 import { FaLinkedin, FaDiscord, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const copyrightDate = new Date().getFullYear();
 
@@ -9,20 +10,24 @@ export default function Footer() {
   return (
     <footer className="font-['Geist'] bg-cardsColor pb-10 mt-6">
       <div className="footer lg:max-w-7xl mx-auto p-10 text-base-content md:items-center flex flex-col md:flex-row justify-center items-start">
-        <aside className="flex flex-col mx-auto sm:!ml-0 md:!mr-8 text-center sm:text-start mb-4 sm:mb-0">
-        <a href="/home">
-          <img
-          src={dynamicLogo}
-          alt="logo"
-          style={{ maxHeight: "7rem", maxWidth: "7rem", marginTop: "-.90rem" }}
-          />
-          </a>
-          <h2 className="text-2xl my-3">Digitomize</h2>
+        <aside className="flex flex-col items-center justify-center mx-auto sm:mx-0 sm:ml-0 md:mr-8 text-center sm:text-start mb-4 sm:mb-0">
+          <Link
+            to="/home"
+            className="flex flex-col items-center justify-center"
+          >
+            <img
+              src={dynamicLogo}
+              alt="logo"
+              className="max-w-[7rem] max-h-[7rem] mb-2"
+            />
+            <h2 className="text-2xl my-3">Digitomize</h2>
+          </Link>
 
           <p className="text-base-content text-md max-w-[230px] mb-5">
             Explore upcoming coding contests and dynamically create developer
             portfolios
           </p>
+
           <GitHubButton
             href="https://github.com/digitomize/digitomize"
             data-color-scheme="no-preference: dark_high_contrast; light: dark_high_contrast; dark: light;"
@@ -32,12 +37,22 @@ export default function Footer() {
           >
             Star
           </GitHubButton>
-          <div className="flex justify-center mt-px-2" style={{marginTop:'5px'}}>
-              <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
-              <img src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg" alt="Deploys by Netlify" className="w-[18] h-10" /> {/*added here*/}
+
+          <div className="flex justify-center mt-2">
+            <a
+              href="https://www.netlify.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg"
+                alt="Deploys by Netlify"
+                className="w-[18] h-10"
+              />
             </a>
           </div>
         </aside>
+
         <div className="lg:flex-1 flex flex-col sm:flex-row sm:flex-wrap mx-auto justify-between gap-8 mb-4 sm:mb-0">
           <nav className="flex justify-center items-center sm:!items-start flex-col gap-3 w-full sm:!w-[45%] xl:!w-fit">
             <header className="text-white font-medium text-2xl mb-2">
