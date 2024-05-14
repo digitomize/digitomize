658a25f4c0bae8ffce2b00d773a2ee0118477467
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-05-14 10:52:47 +0530

    added protected route for unverified users

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useUserAuth } from "@context/UserAuthContext";
+import { auth } from "../firebase";
 
 function ProtectedRoute() {
   const { user } = useUserAuth();
-  return user ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login?message=Please login first!" />
-  );
+  if (user) {
+    if (auth && auth?.currentUser?.emailVerified) {
+      return <Outlet />;
+    } else if (auth && !auth?.currentUser?.emailVerified) {
+      return (
+        <Navigate to="/resend-email-verification?message=Please verify your email" />
+      );
+    } else {
+      return <Navigate to="/login?message=Please login first!" />;
+    }
+  } else {
+    return <Navigate to="/login?message=Please login first!" />;
+  }
 }
 
 export default ProtectedRoute;
