ca5fd1ecb1ba394b5ee9c60a3b7c6ff03e2a71ee
“[keshav <[kesusinha17@gmail.com]>
2024-05-22 00:59:06 +0530

    setup-envs upated

--- backend/setup-env.js
+++ backend/setup-env.js
@@ -6,7 +6,7 @@ const questions = [
         type: 'input',
         name: 'MONGODB_URL',
         message: 'Enter MongoDB URL:',
-        default: 'mongodb://localhost:27017/digitomize',
+        default: 'mongodb://127.0.0.1:27017/digitomize',
         validate: function (value) {
             if (value.trim()) {
                 return true;
@@ -42,7 +42,13 @@ const questions = [
         type: 'confirm',
         name: 'CONTESTS',
         message: 'Enable contests?',
-        default: false
+        default: true
+    },
+    {
+        type: 'confirm',
+        name: 'HACKATHONS',
+        message: 'Enable hackathon?',
+        default: true
     },
     {
         type: 'confirm',
@@ -66,31 +72,42 @@ const questions = [
     {
         type: 'input',
         name: 'FIREBASE_CREDENTIALS',
-        message: 'Enter Firebase credentials as a single object:',
-        default: '{}',
+        message: 'Enter Firebase credentials JSON as a single object :',
+
         validate: function (value) {
             try {
-                JSON.parse(value);
+                const credentials = JSON.parse(value);
+                if (!credentials.type || !credentials.project_id || !credentials.private_key) {
+                    return 'Invalid Firebase credentials. Missing required properties.';
+                }
                 return true;
             } catch (e) {
                 return 'Please enter a valid JSON object.';
             }
         }
-    }
+    },
+    {
+        type: 'input',
+        name: 'NOVU_API_KEY',
+        message: 'Enter the NOVU Api Key:',
+        default: '',
+    },
 ];
 
 async function setup() {
     try {
         const answers = await inquirer.prompt(questions);
+
         const envContent = `TEST=Env file connected\n` +
             Object.entries(answers)
                 .map(([key, value]) => `${key}=${typeof value === 'boolean' ? value : value}`)
                 .join('\n');
+
         fs.writeFileSync('.env', envContent);
         console.log('Environment variables have been set successfully.');
     } catch (error) {
         console.error('Error occurred during setup:', error);
     }
-}
 
+}
 setup();
--- client/setup-env.js
+++ client/setup-env.js
@@ -1,114 +1,125 @@
-import inquirer from 'inquirer';
-import fs from 'fs';
+import inquirer from "inquirer";
+import fs from "fs";
 
 const questions = [
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_BACKEND_URL',
-    message: 'Enter backend URL:',
-    default: 'http://localhost:4001',
+    type: "input",
+    name: "VITE_REACT_APP_BACKEND_URL",
+    message: "Enter backend URL:",
+    default: "http://localhost:4001",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for backend URL.';
-    }
+      return "Please enter a value for backend URL.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_FRONTEND_URL',
-    message: 'Enter frontend URL:',
-    default: 'http://localhost:5173',
+    type: "input",
+    name: "VITE_REACT_APP_FRONTEND_URL",
+    message: "Enter frontend URL:",
+    default: "http://localhost:5173",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for frontend URL.';
-    }
+      return "Please enter a value for frontend URL.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_API_KEY',
-    message: 'Enter API key:',
+    type: "input",
+    name: "VITE_REACT_APP_API_KEY",
+    message: "Enter Firebase API key:",
+    default:"AIzaSyALK7O1zL_8kftC-fG8Ke0_KF1VEb--dTw",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for API key.';
-    }
+      return "Please enter a value for Firebase API key.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_AUTH_DOMAIN',
-    message: 'Enter authentication domain:',
+    type: "input",
+    name: "VITE_REACT_APP_AUTH_DOMAIN",
+    message: "Enter Firebase authentication domain:",
+    default:"digitomize-testers.firebaseapp.com",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for authentication domain.';
-    }
+      return "Please enter a value for Firebase authentication domain.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_PROJECT_ID',
-    message: 'Enter project ID:',
+    type: "input",
+    name: "VITE_REACT_APP_PROJECT_ID",
+    message: "Enter Firebase project ID:",
+    default:"digitomize-testers",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for project ID.';
-    }
+      return "Please enter a value for Firebase project ID.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_STORAGE_BUCKET',
-    message: 'Enter storage bucket:',
+    type: "input",
+    name: "VITE_REACT_APP_STORAGE_BUCKET",
+    message: "Enter Firebase storage bucket:",
+    default:"digitomize-testers.appspot.com",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for storage bucket.';
-    }
+      return "Please enter a value for Firebase storage bucket.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_MESSAGING_SENDER_ID',
-    message: 'Enter messaging sender ID:',
+    type: "input",
+    name: "VITE_REACT_APP_MESSAGING_SENDER_ID",
+    message: "Enter Firebase messaging sender ID:",
+    default:"1032745553541",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for messaging sender ID.';
-    }
+      return "Please enter a value for Firebase messaging sender ID.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_APP_ID',
-    message: 'Enter app ID:',
+    type: "input",
+    name: "VITE_REACT_APP_APP_ID",
+    message: "Enter Firebase app ID:",
+    default:"1:1032745553541:web:8a4647c8754edb051123ec",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for app ID.';
-    }
+      return "Please enter a value for Firebase app ID.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_MEASUREMENT_ID',
-    message: 'Enter measurement ID:',
+    type: "input",
+    name: "VITE_REACT_APP_MEASUREMENT_ID",
+    message: "Enter Firebase measurement ID:",
+    default:"G-XVFS5HD03Z",
     validate: function (value) {
       if (value.trim()) {
         return true;
       }
-      return 'Please enter a value for measurement ID.';
-    }
+      return "Please enter a value for Firebase measurement ID.";
+    },
   },
   {
-    type: 'input',
-    name: 'VITE_REACT_APP_FORMBRICKS_API_KEY',
-    message: 'Enter Formbricks api key:',
-
-  }
+    type: "input",
+    name: "VITE_REACT_APP_FORMBRICKS_API_KEY",
+    message: "Enter Formbricks api key:",
+  },
+  {
+    type: "input",
+    name: "VITE_REACT_APP_VAPID_KEY",
+    message: "Enter VapID Key:",
+  },
 ];
 
 async function setup() {
@@ -116,11 +127,11 @@ async function setup() {
     const answers = await inquirer.prompt(questions);
     const envContent = Object.entries(answers)
       .map(([key, value]) => `${key}=${value}`)
-      .join('\n');
-    fs.writeFileSync('.env', envContent);
-    console.log('Environment variables have been set successfully.');
+      .join("\n");
+    fs.writeFileSync(".env", envContent);
+    console.log("Environment variables have been set successfully.");
   } catch (error) {
-    console.error('Error occurred during setup:', error);
+    console.error("Error occurred during setup:", error);
   }
 }
 
