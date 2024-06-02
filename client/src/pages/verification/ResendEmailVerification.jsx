ef468a3ffe936d37d615199bcc3b10376b3b03e0
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-06-02 21:43:18 +0530

    minor changes

--- client/src/pages/verification/ResendEmailVerification.jsx
+++ client/src/pages/verification/ResendEmailVerification.jsx
@@ -71,11 +71,11 @@ const ResendEmailVerification = () => {
                 className="relative inline-flex items-center justify-center px-6 py-2 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group min-w-2/5"
               >
                 <span
-                  className={`absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-[#4285f4] rounded-md group-hover:mt-0 group-hover:ml-0`}
+                  className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-[#4285f4] rounded-md group-hover:mt-0 group-hover:ml-0"
                 />
                 <span className="absolute inset-0 w-full h-full bg-white rounded-md "/>
                 <span
-                  className={`absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-[#4285f4] rounded-md opacity-0 group-hover:opacity-100 `}
+                  className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-[#4285f4] rounded-md opacity-0 group-hover:opacity-100"
                 />
                 <span className="relative text-black transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
                  {btnState ? 'Sending...' : 'Resend Email'}
--- client/src/pages/verification/VerifyEmailPage.jsx
+++ client/src/pages/verification/VerifyEmailPage.jsx
@@ -57,7 +57,6 @@ const VerifyEmailPage = () => {
       isMounted = false;
     };
   }, [isMounted]);
-  console.log(isMounted);
   if (isLoading) {
     return (
       <div className="h-screen flex items-center justify-center z-30">
@@ -86,11 +85,11 @@ const VerifyEmailPage = () => {
             className="relative inline-flex items-center justify-center px-6 py-2 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group min-w-2/5"
           >
              <span
-                  className={`absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-[#4285f4] rounded-md group-hover:mt-0 group-hover:ml-0`}
+                  className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-[#4285f4] rounded-md group-hover:mt-0 group-hover:ml-0"
                 />
                 <span className="absolute inset-0 w-full h-full bg-white rounded-md "/>
                 <span
-                  className={`absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-[#4285f4] rounded-md opacity-0 group-hover:opacity-100 `}
+                  className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-[#4285f4] rounded-md opacity-0 group-hover:opacity-100"
                 />
                 <span className="relative text-black transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
                   Login
@@ -113,11 +112,11 @@ const VerifyEmailPage = () => {
             className="relative inline-flex items-center justify-center px-6 py-2 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group min-w-2/5"
           >
              <span
-                  className={`absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-[#4285f4] rounded-md group-hover:mt-0 group-hover:ml-0`}
+                  className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-[#4285f4] rounded-md group-hover:mt-0 group-hover:ml-0"
                 />
                 <span className="absolute inset-0 w-full h-full bg-white rounded-md "/>
                 <span
-                  className={`absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-[#4285f4] rounded-md opacity-0 group-hover:opacity-100 `}
+                  className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-[#4285f4] rounded-md opacity-0 group-hover:opacity-100 "
                 />
                 <span className="relative text-black transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
                   Home
