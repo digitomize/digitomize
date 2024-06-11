2358323dd26387f80ee995898a4f0473ea076fcc
Aadithya-J <jlaadithya@gmail.com>
2024-06-11 20:33:22 +0530

    added challenge controller and db sync

---
+++ backend/challenges/controllers/challengeController.js
@@ -0,0 +1,37 @@
+import { AllChallenge } from "../models/challenge.js";
+
+let allChallengeList = []; // Variable to store challenges in memory
+
+// Function to fetch challenges from MongoDB and update the allChallengeList variable
+
+async function updateChallenges () {
+  try {
+    console.log("┌──────────────────────────────────┐");
+    console.log("│ Retrieving Data | MongoDB to App".padEnd(35) + "│");
+    console.log("└──────────────────────────────────┘");
+
+    // Fetch challenges from MongoDB (without id, createdAt and updatedAt)
+    const fetchedChallenges = await AllChallenge.find().select(
+      "-_id -createdAt -updatedAt -__v",
+    );
+
+    // Sorting challenges
+    fetchedChallenges.sort((a, b) => a.startTimeUnix - b.startTimeUnix);
+
+    // Update the allChallengeList variable
+    allChallengeList = fetchedChallenges;
+
+    console.log("===============================================");
+    console.log("Challenges variable updated successfully. | MongoDb to App");
+    console.log("===============================================");
+  } catch (error) {
+    console.error("Error fetching challenges:", error);
+  }
+}
+
+// Function to return allChallengeList
+async function getChallengeList () {
+  return await allChallengeList;
+}
+
+export { updateChallenges, getChallengeList };
