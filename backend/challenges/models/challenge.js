0eba223acbe04cba2b10f37d0c38c9807478638d
Aadithya-J <jlaadithya@gmail.com>
2024-06-11 16:12:48 +0530

    added schema for challenges

---
+++ backend/challenges/models/challenge.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+// Challenge Schema
+
+const challengeSchema = new mongoose.Schema(
+  {
+    host: {
+      type: String,
+      lowercase: true,
+      required: [true, "Host is required."],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+    vanity: {
+      type: String,
+      required: [true, "Vanity is required."],
+      lowercase: true,
+    },
+    url: {
+      type: String,
+      required: [true, "URL is required."],
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+    },
+    startTimeUnix: {
+      type: Number,
+      required: [true, "Start time is required."],
+    },
+    endTimeUnix: {
+      type: Number,
+      required: [true, "End time is required."],
+    },
+  },
+);
+
+const AllChallenge = mongoose.model("AllChallenge",challengeSchema, "allchallenges");
+
+export { AllChallenge };
